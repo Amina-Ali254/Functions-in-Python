e90118772216da52b0fb2c226431a1906776548b
Amina-Ali254 <amina.ali254@gmail.com>
2024-01-23 15:47:51 +0300

    Assignments - User defined functions

---
+++ user_defined_functions/assisgnment.py
@@ -0,0 +1,16 @@
+# Create a function that divides two numbers?
+def divide_two_numbers(a, b):
+    return a / b
+print(divide_two_numbers(10, 2))
+
+# Create a function that subtracts two numbers?
+def subtract_two_numbers(a, b):
+    return a - b
+print(subtract_two_numbers(10, 5))  
+
+# Create a function that calculates the area of circle?
+import math
+def area_of_circle(radius):
+    return math.pi * radius ** 2
+radius = 5
+print("Area of the circle with radius", radius, "is:", area_of_circle(radius))
